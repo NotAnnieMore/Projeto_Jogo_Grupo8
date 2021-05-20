4c7f75c1e5c5d345b692c5820d69799205d491e2
IvoCM <200100280@esg.ipsantarem.pt>
2021-05-20 13:45:11 +0100

    Projeto

---
+++ Java/Jogo8/src/Prato.java
@@ -0,0 +1,21 @@
+
+public class Prato {
+
+	//========== ATRIBUTOS ==========// 
+	
+	
+	
+	//========== CONSTRUTOR - DEFAULT / ARGS ==========// 
+	
+	
+	
+	//========== ACESSORES - ACESSOS ==========// 
+	
+	
+	
+	//========== COMPORTAMENTOS ==========// 
+	
+	
+	
+	//========== METODOS COMPLEMENTARES ==========//
+}
